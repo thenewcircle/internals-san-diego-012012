0cea32e6cc0f2cfe9b47bcd1ddecee4aa06ca5a0
Marko Gargenta <marko@marakana.com>
2012-01-24 15:50:37 -0800

    Initial checkin.

---
+++ QFib/jni/fib.c
@@ -0,0 +1,12 @@
+#include "com_qualcomm_fib_FibLib.h"
+
+jlong fibR(jlong n) {
+	if(n==0) return 0;
+	if(n==1) return 1;
+	return fibR(n-1)+fib(n-2);
+}
+
+JNIEXPORT jlong JNICALL Java_com_qualcomm_fib_FibLib_fibNR
+  (JNIEnv *env, jclass clazz, jlong n) {
+	return fibR(n);
+}
---
+++ QFib/src/com/qualcomm/fib/FibLib.java
@@ -0,0 +1,12 @@
+package com.qualcomm.fib;
+
+public class FibLib {
+
+	public static long fibJR(long n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibJR(n-1)+fibJR(n-2);
+	}
+	
+	public static native long fibNR(long n);
+}
